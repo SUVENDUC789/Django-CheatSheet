d9e870a4289c6084feed02bf28537c879480f0b3
Suvendu <suvendu.chok@gmail.com>
2022-09-24 20:02:13 +0530

    FormApiView - HttpResponseRedirect class added and redirect submit file

--- FormApiView/FormApiView/urls.py
+++ FormApiView/FormApiView/urls.py
@@ -18,5 +18,6 @@ from django.urls import path
 from home import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('submit/', views.submit, name='submit')
 ]
--- FormApiView/home/views.py
+++ FormApiView/home/views.py
@@ -1,12 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
+from django.http import HttpResponseRedirect
 from home.forms import StudentReg
 # Create your views here.
+
+def submit(request):
+    return render(request,'submit.html')
 
 def home(request):
     if request.method=='POST':
         f=StudentReg(request.POST)
         if f.is_valid():
             print(f.cleaned_data)
+            print("Name : ",f.cleaned_data['name'])
+            print("Email : ",f.cleaned_data['email'])
+            print("Password : ",f.cleaned_data['password'])
+            # return render(request,'submit.html',{'name':f.cleaned_data['name']})
+            return HttpResponseRedirect("/submit/")
     else:
         f=StudentReg()
 

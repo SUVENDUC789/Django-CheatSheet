8b53cea657d26b2031ddcf412c8259530bbf4e82
Suvendu <suvendu.chok@gmail.com>
2022-10-20 14:40:21 +0530

    User auth 1

--- UserAuthentication/UserAuthentication/urls.py
+++ UserAuthentication/UserAuthentication/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from home import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home')
 ]
